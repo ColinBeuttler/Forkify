30f1ed6c32205c3b9c150ca4f9fc17a4b0f9fe5c
Colin Beuttler <beutt102@mail.chapman.edu>
2021-09-28 13:11:12 -0700

    gh ff

--- dist/index.0e47d5c3.js
+++ dist/index.0e47d5c3.js
@@ -462,7 +462,19 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch('https://forkify-api.herokuapp.com/api/get?rId=47746');
+        const data = await res.json();
+        if (!res.ok) throw new error(`${data.message}`);
+        console.log(res, data);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=index.0e47d5c3.js.map
